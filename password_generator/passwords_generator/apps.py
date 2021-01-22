1c69c23cc56f5b72667161f882c1c507db226bc6
bvbkuba <sikora.maciej1992@gmail.com>
2021-01-22 01:29:01 +0100

    test

---
+++ password_generator/passwords_generator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PasswordsGeneratorConfig(AppConfig):
+    name = 'passwords_generator'
---
+++ password_generator/passwords_generator/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+# class ModelPassword(models.Model):
+#     CHOICES = {
+#         (8,8)
+#         (10,10)
+#         (15,15)
+#     }
+#     password =  models.CharField()
+#     password_length = models.CharField(choices=CHOICES)
+
